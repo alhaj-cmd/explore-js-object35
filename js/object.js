39834e53d0ea253400d1569ac0292ac79f6f67c6
github_username <you@domain.com>
2022-09-01 11:07:33 +0600

    first commit

---
+++ js/object.js
@@ -0,0 +1,8 @@
+// create object using literal
+const player = {};
+player.name = "ami sokale uti na";
+player.specialty ='batsman';
+player.bat= function(){
+    console.log('swing your bat');
+}
+console.log('player');
